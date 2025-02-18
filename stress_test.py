2906b078c3105a1df2d9638d9fd3695ef8930738
Timi Taiwo <ttaiwo73@gmail.com>
2025-02-18 14:07:39 +0000

    initial commit with project framwork

---
+++ stress_test.py
@@ -0,0 +1,52 @@
+from tests import *
+
+prompt = """
+1. Press 1 for Memory Stress Testing
+2. Press 2 for Disk Stress Testing
+3. Press 3 for Network Stress Testing
+4. Press 4 for CPU Stress Testing
+5. Press 5 for MySQL Stress Testing
+
+Press Ctrl/Super + C to cancel
+"""
+
+if __name__=="__main__":
+    
+    print("\nWelcome to stress test. Select the below option to stress test your device", end="")
+
+    while True:
+        print(prompt)
+
+        user_selection = input("Which stress test do you want to carry out? ")
+
+        print()
+
+        if not user_selection.isdigit():
+            print("Please pass in an number greater than zero")
+            continue
+
+        user_selection = int(user_selection)
+        lower_bound = 1
+        upper_bound = 5
+        if user_selection < lower_bound or  user_selection > upper_bound:
+            print(f"Invalid number selected. Please select one between {lower_bound} and {upper_bound}")
+            continue
+
+        match user_selection:
+            case 1:
+                memory_stress_test()
+            case 2:
+                disk_stress_test()
+            case 3:
+                network_stress_test()
+            case 4:
+                cpu_stress_test()
+            case 5:
+                mysql_stress_test()
+
+        next_round = input("\nFinished stress test. Another round? yes/no ")
+        if next_round=="y" or next_round=="yes":
+            continue
+
+        print("\ny/yes not detected. Program terminating")
+        break
---
+++ tests/__init__.py
@@ -0,0 +1,15 @@
+
+def memory_stress_test():
+    print("Does memory stress test")
+
+def disk_stress_test():
+    print("Does disk stress test")
+
+def network_stress_test():
+    print("Does network stress test")
+
+def cpu_stress_test():
+    print("Does cpu stress test")
+
+def mysql_stress_test():
+    print("Does MySQL stress test")
